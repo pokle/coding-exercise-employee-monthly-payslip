44430146d6765534dd02b4342520526cc7d74327
Tushar Pokle <tushar.pokle@gmail.com>
2014-09-20 21:42:32 +1000

    Generated an empty leiningen app template

---
+++ test/employee_monthly_payslip/core_test.clj
@@ -0,0 +1,7 @@
+(ns employee-monthly-payslip.core-test
+  (:require [clojure.test :refer :all]
+            [employee-monthly-payslip.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
